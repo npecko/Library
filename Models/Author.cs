afbe8216d19dceb00279e92664983079376b0c4c
npecko <5827002+npecko@users.noreply.github.com>
2020-07-21 19:51:32 +0300

    Add files via upload

---
+++ Models/Author.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Library
+{
+    public class Author
+    {
+        public int AuthorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MainGenre { get; set; }
+        public int BooksWritten { get; set; }
+        public ICollection<Relation> Relations { get; set; }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Library
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; } 
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public ICollection<Relation> Relations { get; set; }
+    }
+}
