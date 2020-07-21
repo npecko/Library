d383065e26e589bd4d51fa1f5ce937f2d33fc878
npecko <5827002+npecko@users.noreply.github.com>
2020-07-21 19:51:53 +0300

    Add files via upload

---
+++ Models/Relation.cs
@@ -0,0 +1,11 @@
+﻿namespace Library
+{
+    public class Relation
+    {
+        public int RelationID { get; set; }
+        public int BookID { get; set; }
+        public int AuthorID { get; set; }
+        public Author Author { get; set; }
+        public Book Book { get; set; }
+    }
+}
